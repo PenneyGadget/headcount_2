254cf5a241d24ffdaa2b39b97ee977569681ceee
PenneyGadget <dreamofgreen@gmail.com>
2015-11-18 17:38:38 -0700

    added district tests

--- test/district_test.rb
+++ test/district_test.rb
@@ -6,11 +6,9 @@ class DistrictTest < Minitest::Test
 
   def setup
     @dr = DistrictRepository.new
-    @dr.load_data({ :enrollment =>
-                    { :kindergarten =>           "./data/Kindergartners in full-day program.csv",
-                      :high_school_graduation => "./data/High school graduation rates.csv"
-                    }
-                  })
+    @dr.load_data({ :enrollment =>          { :kindergarten => "./data/Kindergartners in full-day program.csv" },
+                    :statewide_testing =>   { :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv" },
+                    :economic_profile =>    { :median_household_income => "./data/Median household income.csv" }})
   end
 
   def test_district_class_exists
@@ -25,6 +23,14 @@ class DistrictTest < Minitest::Test
 
   def test_set_enrollment_method_returns_an_enrollment_object
     assert_equal Enrollment, @dr.find_by_name("Colorado").enrollment.class
+  end
+
+  def test_set_statewide_test_method_returns_a_statewide_test_object
+    assert_equal StatewideTest, @dr.find_by_name("Academy 20").statewide_test.class
+  end
+
+  def test_set_economic_profile_method_returns_an_economic_profile_object
+    assert_equal EconomicProfile, @dr.find_by_name("Colorado").economic_profile.class
   end
 
 end
